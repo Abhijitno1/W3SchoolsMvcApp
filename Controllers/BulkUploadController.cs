39b31b8db9c8bc0b53bc3a5fbba699b1d5ef5ed6
Abhijitno1 <abhijitno1@yahoo.com>
2016-05-28 16:35:50 +0530

    Implemented Bulk upload of movies using CSV reader
    Converted project to VS2013 solution
    Implemented Bulk upload of movies using CSV reader

---
+++ Controllers/BulkUploadController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using W3SchoolsMvcApp.Services;
+using W3SchoolsMvcApp.Models;
+
+namespace W3SchoolsMvcApp.Controllers
+{
+    public class BulkUploadController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //ToDo: Convert to async method
+        public ActionResult UploadCSV(HttpPostedFileBase fileUpload)
+        {
+            if (fileUpload != null)
+            {
+                //fileUpload.SaveAs(Path.Combine(Server.MapPath("~/Assets/"), fileUpload.FileName));
+                if (!fileUpload.FileName.ToLower().EndsWith(".csv"))
+                {
+                    ModelState.AddModelError("fileUpload", "Please select a CSV file to upload");
+                    return View("Index");
+                }
+                CSVImporter importer = new CSVImporter();
+                List<Movie> newmovies = importer.ParseCSVFile(fileUpload.InputStream);
+                importer.SaveMovies2DB(newmovies);
+                return RedirectToAction("Index", "Movie");
+            }
+            else
+            {
+                ModelState.AddModelError("fileUpload", "Please select a file to upload");
+                return View("Index");
+            }
+        }
+    }
+}
---
+++ Services/CSVImporter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using W3SchoolsMvcApp.Models;
+using LumenWorks.Framework.IO.Csv;
+using System.IO;
+
+namespace W3SchoolsMvcApp.Services
+{
+    public class CSVImporter
+    {
+
+        public List<Movie> ParseCSVFile(Stream inputStream)
+        {
+            List<Movie> output = new List<Movie>();
+            bool HAS_HEADERS = true;
+            using (CsvReader reader = new CsvReader(new StreamReader(inputStream), HAS_HEADERS))
+            {
+                while (reader.ReadNextRecord())
+                {
+                    Movie newMovie = new Movie();
+                    newMovie.Title = reader["Title"].Trim();
+                    newMovie.Director = reader["Director"].Trim();
+                    DateTime releaseDate;
+                    if (DateTime.TryParse(reader["ReleaseDate"].Trim(), out releaseDate))
+                        newMovie.ReleaseDate = releaseDate;
+
+                    output.Add(newMovie);
+                }
+            }
+            return output;
+        }
+
+        public void SaveMovies2DB(IEnumerable<Movie> movies)
+        {
+            MoviesDbEntities ntt = new MoviesDbEntities();
+            foreach (Movie movie in movies)
+                ntt.Movies.Add(movie);
+            ntt.SaveChanges();
+        }
+    }
+}
